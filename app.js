3a1d06e44571e7b8c4d6e4fda32660f1e9379c9f
cybermechanicjourney <z4zywyrzwv@privaterelay.appleid.com>
2026-04-23 00:54:49 +0200

    Fix login condition syntax in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function login() {
   const u = document.getElementById("user").value;
   const p = document.getElementById("pass").value;
 
-  if (u === "NIYO" && p === "TimeLessJourney") {
+ {
     document.getElementById("login").style.display = "none";
     document.getElementById("app").style.display = "block";
   } else {
